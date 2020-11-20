bfc04f6e714f0291e1d26ae0ddf4a1831bec6f0d
root <mkolybko@gmail.com>
2020-11-20 19:06:43 +0000

    Added more limitations

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 import discord
 import requests
+import time
+
 from datetime import datetime
 
 from getQuotes import getRandom
 
 class DiscordClient(discord.Client):
-	def __init__(self):
-		self.tlimit = time.time()
 
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+        self.tlimit = time.time()
 
     async def on_message(self, message):
         text = message.content
@@ -26,7 +27,8 @@ class DiscordClient(discord.Client):
         if user.id == 257130372213506048:
             return
         emoji = reaction.emoji
-        if time.time() - self.tlimit > 20 &&  reaction.custom_emoji and (emoji.id == 779415235735322656 or emoji.id == 779415413280473108):
+        print(reaction.message.channel.name)
+        if reaction.message.channel.name == "general" and time.time() - self.tlimit > 20 and reaction.custom_emoji and (emoji.id == 779415235735322656 or emoji.id == 779415413280473108):
             await reaction.message.channel.send(getRandom(False))
             self.tlimit = time.time()
         # if emoji.is_custom_emoji() and emoji.id == 779413738054352946:
