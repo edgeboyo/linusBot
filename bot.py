edf15f316bc1ea8a1d0f92bf621fa84d7225e5b4
edgeboyo <mkolybko@gmail.com>
2020-11-20 18:43:38 +0000

    Added simple reaction based system

--- bot.py
+++ bot.py
@@ -17,3 +17,11 @@ class DiscordClient(discord.Client):
             await message.channel.send(getRandom(False))
         if text == "LONGRANT":
             await message.channel.send(getRandom(True))
+
+    async def on_reaction_add(self, reaction, user):
+    	if user.id == 257130372213506048:
+    		return
+        emoji = reaction.emoji
+        if reaction.custom_emoji and (emoji.id == 779415235735322656 or emoji.id == 779415413280473108):
+            await reaction.message.channel.send(getRandom(False))
+        # if emoji.is_custom_emoji() and emoji.id == 779413738054352946:
