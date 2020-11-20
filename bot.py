92a8fca014f82fd95f7ad0661285ce387792285b
edgeboyo <mkolybko@gmail.com>
2020-11-20 18:28:18 +0000

    Forked out master

--- bot.py
+++ bot.py
@@ -17,13 +17,3 @@ class DiscordClient(discord.Client):
             await message.channel.send(getRandom(False))
         if text == "LONGRANT":
             await message.channel.send(getRandom(True))
-    async def on_raw_reaction_add(self, payload):
-        print("I saw react XD")
-
-    async def on_reaction_add(self, reaction, user):
-        reactList = reaction.message.reactions
-        print(reactList)
-        for i in reactList:
-            if i.count > 1:
-                await reaction.message.channel.send("Whoa there!")
-                await reaction.message.delete()
